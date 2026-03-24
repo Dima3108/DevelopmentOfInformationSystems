e803168c1ddb3574cd252034ded9c60945c65da2
Dima <73338242+Dima3108@users.noreply.github.com>
2026-03-24 11:53:03 +0300

    upfate

---
+++ task3/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Data.Common;
+using System.Data.SqlTypes;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "users1", "users2", "users3" };
+            string[] mails = { "@mail.yand", "rt@mail.ru", "pr@mail.com" };
+            string command = "INSERT INTO users(name,email) ";
+            int LEN = 10000;
+            for(int i = 0; i < LEN; i++)
+            {
+                command += $"('{names[i % names.Length]}{i}','us{i}{mails[i%mails.Length]}')";
+                if (i != LEN)
+                {
+                    command += ",";
+                }
+                else
+                {
+                    command += ";";
+                }
+            }
+            File.WriteAllText("com.sql",command);
+        }
+    }
+}
---
+++ task3/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "Host=localhost;Username=postgres;Password=postpass;Database=Products";
+            await using var dataSource = NpgsqlDataSource.Create(connectionString);
+            const string tablename = "myschema2.accounts";
+            using(var cmd = dataSource.CreateCommand($"SELECT COUNT(ID) FROM {tablename} ;"))
+            {
+                Console.WriteLine(Convert.ToInt32(await cmd.ExecuteScalarAsync()));
+            }
+        }
+    }
+}
